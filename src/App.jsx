461263e79dad8da258822387bbe8e46b3249a080
Pa1wast <135978194+Pa1wast@users.noreply.github.com>
2024-05-03 02:50:49 +0300

    Setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+const router = createBrowserRouter([
+	{
+		element: <Login />,
+		path: 'login',
+	},
+	{
+		element: <Dashboard />,
+		path: '/',
+	},
+]);
+
 function App() {
-	return <div>ITC</div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 function Login() {
-	return <div></div>;
+	return <div>Log in</div>;
 }
 
 export default Login;
