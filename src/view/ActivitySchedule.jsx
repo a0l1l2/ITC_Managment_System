18cf2690dd2036044a554f728234fd5389cfb48d
Ali Mohammed <153657019+a0l1l2@users.noreply.github.com>
2024-05-17 15:12:56 +0300

    update(Ali)

--- src/view/ActivitySchedule.jsx
+++ src/view/ActivitySchedule.jsx
@@ -1,87 +1,72 @@
-import toast, { Toaster } from 'react-hot-toast';
-import styles from './ActivitySchedule.module.css';
-import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { getActivities } from '../model/getActivities';
-import { ClipLoader } from 'react-spinners';
-import { uploadActivities } from '../model/uploadActivities';
+import toast, { Toaster } from "react-hot-toast";
+import styles from "./ActivitySchedule.module.css";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getActivities } from "../model/getActivities";
+import { ClipLoader } from "react-spinners";
+import { uploadActivities } from "../model/uploadActivities";
 
 function ActivitySchedule() {
-	const navigate = useNavigate();
-	const [activities, setActivities] = useState([]);
+  const navigate = useNavigate();
+  const [activities, setActivities] = useState([]);
 
-	useEffect(() => {
-		async function fetchActivities() {
-			const fetchedActivities = await getActivities();
+  useEffect(() => {
+    async function fetchActivities() {
+      const fetchedActivities = await getActivities();
 
-			setActivities(fetchedActivities);
-		}
+      setActivities(fetchedActivities);
+    }
 
-		fetchActivities();
-	}, []);
+    fetchActivities();
+  }, []);
 
-	return (
-		<div>
-			<Toaster />
+  return (
+    <div>
+      <Toaster />
 
-			<h1>Activity Schedule</h1>
+      <h1>Activity Schedule</h1>
 
-			<div className={styles.table}>
-				<div className={styles.header}>
-					<p>Activity</p>
-					<p>Team</p>
-					<p>Description</p>
-					<p>Date</p>
-				</div>
+      <div className={styles.table}>
+        <div className={styles.header}>
+          <p>Activity</p>
+          <p>Team</p>
+          <p>Description</p>
+          <p>Date</p>
+        </div>
 
-				<div className={styles.body}>
-					{activities.length ? (
-						activities.map(activity => (
-							<div
-								className={styles.row}
-								key={activity.activity.id}
-							>
-								<p>{activity.activity.title}</p>
-								<p>{activity.teamInfo.name}</p>
-								<p>{activity.activity.description}</p>
-								<p>{activity.activity.date}</p>
-								<button
-									onClick={() =>
-										navigate(
-											`/editactivity/${activity.activity.id}`
-										)
-									}
-								>
-									Change date
-								</button>
-							</div>
-						))
-					) : (
-						<div className={styles.loader}>
-							<ClipLoader />
-						</div>
-					)}
-				</div>
-			</div>
-			<div className={styles.operations}>
-				<button
-					onClick={() =>
-						toast("This feature hasn't been implemented yet")
-					}
-				>
-					Add activity
-				</button>
-				{/* <button
-					onClick={async function fecth() {
-						const error = await uploadActivities();
-						console.log(error);
-					}}
-				>
-					Upload activities(DEV only) (DO NOT CLICK)
-				</button> */}
-			</div>
-		</div>
-	);
+        <div className={styles.body}>
+          {activities.length ? (
+            activities.map((activity) => (
+              <div className={styles.row} key={activity.activity.id}>
+                <p>{activity.activity.title}</p>
+                <p>{activity.teamInfo.name}</p>
+                <p>{activity.activity.description}</p>
+                <p>{activity.activity.date}</p>
+                <button
+                  onClick={() =>
+                    navigate(`/editactivity/${activity.activity.id}`)
+                  }
+                >
+                  Change date
+                </button>
+              </div>
+            ))
+          ) : (
+            <div className={styles.loader}>
+              <ClipLoader />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className={styles.operations}>
+        <button
+          onClick={() => toast("This feature hasn't been implemented yet")}
+        >
+          Add activity
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ActivitySchedule;
--- src/view/Announcements.jsx
+++ src/view/Announcements.jsx
@@ -1,59 +1,59 @@
-import toast, { Toaster } from 'react-hot-toast';
-import styles from './Announcements.module.css';
-import { Link, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { ClipLoader } from 'react-spinners';
-import { getEvents } from '../model/getEvents';
+import toast, { Toaster } from "react-hot-toast";
+import styles from "./Announcements.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ClipLoader } from "react-spinners";
+import { getEvents } from "../model/getEvents";
 
 function Announcments() {
-	const navigate = useNavigate();
-	const [events, setEvents] = useState([]);
+  const navigate = useNavigate();
+  const [events, setEvents] = useState([]);
 
-	useEffect(() => {
-		async function fetchEvents() {
-			const fetchedEvents = await getEvents();
-			setEvents(fetchedEvents);
-		}
+  useEffect(() => {
+    async function fetchEvents() {
+      const fetchedEvents = await getEvents();
+      setEvents(fetchedEvents);
+    }
 
-		fetchEvents();
-	}, []);
+    fetchEvents();
+  }, []);
 
-	return (
-		<div>
-			<Toaster />
+  return (
+    <div>
+      <Toaster />
 
-			<h1>Announcments</h1>
+      <h1>Announcements</h1>
 
-			<div className={styles.table}>
-				<div className={styles.header}>
-					<p>Event</p>
-					<p>Team</p>
-					<p>Description</p>
-					<p>Date</p>
-				</div>
+      <div className={styles.table}>
+        <div className={styles.header}>
+          <p>Event</p>
+          <p>Team</p>
+          <p>Description</p>
+          <p>Date</p>
+        </div>
 
-				<div className={styles.body}>
-					{events.length ? (
-						events.map(event => (
-							<div className={styles.row} key={event.event.id}>
-								<p>{event.event.title}</p>
-								<p>{event.teamInfo.name}</p>
-								<p>{event.event.description}</p>
-								<p>{event.event.date}</p>
-							</div>
-						))
-					) : (
-						<div className={styles.loader}>
-							<ClipLoader />
-						</div>
-					)}
-				</div>
-			</div>
-			<div className={styles.operations}>
-				<Link to="/announceevent">Announce event</Link>
-			</div>
-		</div>
-	);
+        <div className={styles.body}>
+          {events.length ? (
+            events.map((event) => (
+              <div className={styles.row} key={event.event.id}>
+                <p>{event.event.title}</p>
+                <p>{event.teamInfo.name}</p>
+                <p>{event.event.description}</p>
+                <p>{event.event.date}</p>
+              </div>
+            ))
+          ) : (
+            <div className={styles.loader}>
+              <ClipLoader />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className={styles.operations}>
+        <Link to="/announceevent">Announce event</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Announcments;
